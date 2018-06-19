498a5d5bc733878f79dd0e1fd8cd74336eb6c29a
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-19 11:01:12 -0700

    connection config, orm setup

---
+++ config/orm.js
@@ -0,0 +1,40 @@
+const connection = require("./connection.js");
+
+// Object Relational Mapper (ORM)
+
+// The ?? signs are for swapping out table or column names
+// The ? signs are for swapping out other values
+// These help avoid SQL injection
+// https://en.wikipedia.org/wiki/SQL_injection
+const orm = {
+  selectAll: (tableInput, colToSearch, valOfCol) => {
+    let queryString = "SELECT * FROM ?? WHERE ?? = ?";
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  insertOne: (whatToSelect, table, orderCol) => {
+    let queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+    console.log(queryString);
+    connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      if (err) throw err;
+      console.log(result);
+    });
+  },
+  findWhoHasMost: (tableOneCol, tableTwoForeignKey, tableOne, tableTwo) => {
+    let queryString =
+      "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+
+    connection.query(
+      queryString,
+      [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
+      function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      }
+    );
+  }
+};
+
+module.exports = orm;
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const orm = require("./config/orm.js");
 
 const app = express();
 
@@ -11,7 +12,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
  
-require("./app/routing/apiRoutes.js")(app);
-require("./app/routing/htmlRoutes.js")(app);
+// require("./app/routing/apiRoutes.js")(app);
+// require("./app/routing/htmlRoutes.js")(app);
+// Find all the pets ordering by the lowest price to the highest price.
+// orm.selectAndOrder("animal_name", "pets", "price");
+
+// Find a pet in the pets table by an animal_name of Rachel.
+// orm.selectWhere("pets", "animal_name", "Rachel");
+
+// Find the buyer with the most pets.
+// orm.findWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
