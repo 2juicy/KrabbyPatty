5216d9dc3c161d77c1383ce830a467405eaab3f8
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-19 20:14:58 -0700

    removed extra code

--- models/burger.js
+++ models/burger.js
@@ -2,17 +2,17 @@ const orm = require("../config/orm.js");
 
 const burger = {
   selectAll: function(cb) {
-    orm.selectAll("burgers", (res) => {
+    orm.selectAll((res) => {
       cb(res);
     });
   },
   insertOne: function(burgerName, cb) {
-    orm.insertOne("burgers", burgerName, (res) => {
+    orm.insertOne(burgerName, (res) => {
       cb(res);
     });
   },
   updateOne: function(burgerID, cb) {
-    orm.updateOne("burgers", burgerID, (res) => {
+    orm.updateOne(burgerID, (res) => {
       cb(res);
     });
   }
