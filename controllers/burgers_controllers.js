7be3b3fc94d5b5555dbba03b2feb9dd72b9993fb
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-20 11:01:24 -0700

    yay handlebars working (mostly)

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -6,11 +6,8 @@ const burger = require("../models/burger.js");
 
 router.get("/", (req, res) => {
   burger.selectAll(function(data) {
-    let hbsObject = {
-      burgers: data
-    };
-    console.log(hbsObject.burgers[0].burger_name);
-    res.render("index", hbsObject);
+    console.log(data);
+    res.render("index", {burgers: data});
   });
 });
 
