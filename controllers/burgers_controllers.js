f637999c5bd29c8da7fb4fdf28e6cadea76b5b57
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-20 13:57:51 -0700

    added more routes removed comments

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -11,4 +11,26 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/add", (req, res) => {
+  burger.insertOne(function(data) {
+    console.log(data);
+    res.render("index", {burgers: data});
+  });
+});
+
+router.put("/devour/:id", (req, res) => {
+
+  console.log(req.params.id);
+
+  burger.updateOne({
+    devoured: req.body.devoured
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
 module.exports = router;
