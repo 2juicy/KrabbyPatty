77e5721831e0059a3262db470bfcccc77ea81e52
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-20 19:38:51 -0700

    trying different jawsURL

--- config/connection.js
+++ config/connection.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const connection;
 
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_MAUVE_URL);
 } else {
   connection = mysql.createConnection({
     host: "localhost",
