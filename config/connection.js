bafed33f7060e22c9d64e6157163802b6fbaa7ab
JohnnyC <j0hnnywchiang@gmail.com>
2018-06-20 19:42:15 -0700

    one more try at connecting to heroku

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 const mysql = require("mysql");
 let connection;
 
-if (process.env.JAWSDB_URL) {
+if (process.env.JAWSDB_MAUVE_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_MAUVE_URL);
 } else {
   connection = mysql.createConnection({
